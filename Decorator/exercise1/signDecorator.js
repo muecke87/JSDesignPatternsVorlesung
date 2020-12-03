77d0569db030bc9317febecefa7da023605cacc2
samu <sandro.muggli@finnova.com>
2020-12-03 17:03:37 +0100

    solution for ex1 decorator

--- Decorator/exercise1/signDecorator.js
+++ Decorator/exercise1/signDecorator.js
@@ -13,18 +13,24 @@ function Album(price) {
  * verbessere die funktion
  */
 
+/*
+ sicherste variante mit übergabe des contexts
+ ohne context und via "this" würde folgender call fehlschlagen:
+   var getPrice = sign(album.getPrice);
+   console.log(getPrice());
+ weil this jetzt "getPrice" ist und nicht album und dort existiert property "price" ("this.price") nicht.
+ ohne context als parameter kann also this "alles" sein, jenachdem wie die function aufgerufen wird
+ */
 function sign(func, context) {
     return function() {
-        return func.call(this) * 2;
+        return func.call(context) * 2;
     }
 }
 
-// funktioniert
 var album = new Album(20);
-album.getPrice = sign(album.getPrice);
+album.getPrice = sign(album.getPrice, album);
 console.log(album.getPrice());
 
-// funktioniert nicht
 var album = new Album(30);
-var getPrice = sign(album.getPrice);
+var getPrice = sign(album.getPrice, album);
 console.log(getPrice());
