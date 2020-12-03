1de73d12f742885b5032bfc2b328aa8da7d9aaa4
samu <sandro.muggli@finnova.com>
2020-12-03 17:13:38 +0100

    starting point for ex2 spy decorator

---
+++ Decorator/exercise2/spyDecorator.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// zu dekorieren
+function login(username, password) {
+    console.log("logging in with username: " + username);
+}
+
+// decorator
+function spy(func) {
+    function wrapper(...args) {
+        // todo: implementiere hier:
+        // der spy soll alle funktionsaufrufe von "login" im array "calls" speichern und die dabei mitgegebenen parameter
+    }
+    wrapper.calls = [];
+    return wrapper;
+}
+
+login = spy(login);
+login('max', 's#fepw');
+login('bob', '123')
+
+for (let args of login.calls) {
+    console.log('call:' + args.join()); // "call:1,2", "call:4,5"
+}
+
+
+
