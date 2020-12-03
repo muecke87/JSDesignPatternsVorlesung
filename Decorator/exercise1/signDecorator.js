1bdae1f93c30d0ef2ca30fc73f2bce13fdd5e319
samu <sandro.muggli@finnova.com>
2020-12-03 17:02:09 +0100

    starting point for ex1 decorator

---
+++ Decorator/exercise1/signDecorator.js
@@ -0,0 +1,30 @@
+function Album(price) {
+    this.price = price;
+
+    // zu dekorieren (concrete component)
+    this.getPrice = function() {
+        return this.price;
+    }
+}
+
+// decorator
+/**
+ * todo: funktioniert nicht immer, weshalb?
+ * verbessere die funktion
+ */
+
+function sign(func, context) {
+    return function() {
+        return func.call(this) * 2;
+    }
+}
+
+// funktioniert
+var album = new Album(20);
+album.getPrice = sign(album.getPrice);
+console.log(album.getPrice());
+
+// funktioniert nicht
+var album = new Album(30);
+var getPrice = sign(album.getPrice);
+console.log(getPrice());
